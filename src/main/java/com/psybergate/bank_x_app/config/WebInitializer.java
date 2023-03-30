19a5a9c559ed11676f0fe74fa720860c11e17433
Sufyaan Chopdat <sufyaan@psybergate.co.za>
2023-03-30 15:27:54 +0200

    Cleaned WebInitializer class.

--- src/main/java/com/psybergate/bank_x_app/config/WebInitializer.java
+++ src/main/java/com/psybergate/bank_x_app/config/WebInitializer.java
@@ -1,11 +1,8 @@
 package com.psybergate.bank_x_app.config;
 
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Allows deploying this application in a WAR.
@@ -14,17 +11,5 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @ComponentScan(basePackages = "com.psybergate.bank_x_app")
 @SuppressWarnings("unused")
 public class WebInitializer extends SpringBootServletInitializer {
-//
-//  @Bean
-//  @SuppressWarnings("unused")
-//  public WebMvcConfigurer corsConfigurer() {
-//    return new WebMvcConfigurer() {
-//      @Override
-//      @SuppressWarnings("NullableProblems")
-//      public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "POST", "DELETE",
-//            "PUT");
-//      }
-//    };
-//  }
+
 }
